64f18a2d69d5c07a066e7ccb22e7914267dea12b
ying920 <58645318+ying920@users.noreply.github.com>
2020-05-11 23:31:11 +0800

    新增实现现有前端与服务器的连接

---
+++ authentication3/pages/orderTaking/orderTaking.js
@@ -0,0 +1,79 @@
+// pages/orderTaking/orderTaking.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function () {
+    // var items = wx.getStorageSync("items")//将items首先要获取到
+    var self = this
+    wx.request({
+      
+      method: 'GET',
+      'url': 'http://littleeyes.cn:8080/get-all-missed-order',
+      success(res) {
+        console.log(res.data.data),
+        self.setData({
+              list: res.data.data,
+            })
+      }
+    })
+      
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
